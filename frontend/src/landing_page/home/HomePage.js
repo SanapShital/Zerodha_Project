83cf8c957cd6289661d0c54bac291515fea39b09
Shital Tukaram Sanap <132325540+SanapShital@users.noreply.github.com>
2025-06-21 11:27:29 +0000

    home page setup

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,29 +1,26 @@
-
-
-import React from 'react';
-import Stats from './Stats';
-import Awards from './Awards';
-import Hero from './Hero';
-import Pricing from './Pricing';
-import OpenAccount from '../../OpenAccount';
-import Education from './Education';
-import Navbar from '../../Navbar';
-import Footer from '../../Footer';
-
+import React from "react";
+import Stats from "./Stats";
+import Awards from "./Awards";
+import Hero from "./Hero";
+import Pricing from "./Pricing";
+import OpenAccount from "../../OpenAccount";
+import Education from "./Education";
+import Navbar from "../../Navbar";
+import Footer from "../../Footer";
 
 function HomePage() {
-    return ( 
-        <>
-        <Navbar/>
-        <Hero/>
-        <Awards/>
-        <Stats/>
-        <Pricing/>
-        <Education/>
-        <OpenAccount/>
-        <Footer/>
-        </>
-     );
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
