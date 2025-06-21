8606057ae9ff9aa48ad8672b3e56f95098b254f9
Shital Tukaram Sanap <132325540+SanapShital@users.noreply.github.com>
2025-06-21 07:01:29 +0000

    component-setup

---
+++ frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,29 @@
+
+
+import React from 'react';
+import Stats from './Stats';
+import Awards from './Awards';
+import Hero from './Hero';
+import Pricing from './Pricing';
+import OpenAccount from '../../OpenAccount';
+import Education from './Education';
+import Navbar from '../../Navbar';
+import Footer from '../../Footer';
+
+
+function HomePage() {
+    return ( 
+        <>
+        <Navbar/>
+        <Hero/>
+        <Awards/>
+        <Stats/>
+        <Pricing/>
+        <Education/>
+        <OpenAccount/>
+        <Footer/>
+        </>
+     );
+}
+
+export default HomePage;
